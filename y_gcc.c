c8c8fdb569c1303558b07c695064c1f990ab6954
Adam Chambost <AdamJoseph@users.noreply.github.com>
2016-11-28 18:14:18 +1000

    Scope change (to avoid possible DCL30-C violation)
    Changed scope just in case the previous version was a "DCL30-C (Return Values)" violation. (Will need to test and check whether this was the case)

--- y_gcc.c
+++ y_gcc.c
@@ -10,7 +10,6 @@ typedef struct wrap {
 
 typedef void (*callback)( func ) ;
 
-
 void show( uintmax_t value ) {
    printf( "%ju" "\n" , value ) ;
 }
@@ -34,37 +33,32 @@ int main( int argc , char *argv[] ) {
    void factorial_improver( wrap partial , callback cb ) {
          cb( ({ 
                uintmax_t _( uintmax_t n ) {
-                     return n == 0  ? 1 
-                                    : ({
-                                          uintmax_t rv = 1;
-                                          partial._(  
-                                                partial 
-                                              , ({
-                                                   void _( func f ) {
-                                                      rv = n * f( n - 1 );
-                                                   };
-                                                   _;
-                                                })
-                                          );
-                                          rv;
-                                       }) ;
+                  return n == 0  ? 1 
+                                 : ({
+                                       uintmax_t rv = 1;
+                                       partial._(  
+                                             partial 
+                                           , ({
+                                                void _( func f ) {
+                                                   rv = n * f( n - 1 );
+                                                };
+                                                _;
+                                             })
+                                       );
+                                       rv;
+                                    }) ;
                };
                _;
          }) );
-    };
+   };
 
-    func factorial;
+   void fifty( func fx ) {
+      show( fx( 50 ) );
+   };  
 
-    y(   (wrap) { ._ = factorial_improver } 
-       , ({ 
-               void _( func fx ) {
-                  factorial = fx;
-               };
-               _;
-         })
-    );
+   y(    (wrap) { ._ = factorial_improver } 
+       , fifty
+   );
 
-    show( factorial( 50 ) );
-
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
