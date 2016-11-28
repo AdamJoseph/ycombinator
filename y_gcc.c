ada8ede6800932245b29403a8be30c2ba938e53d
Adam Chambost <AdamJoseph@users.noreply.github.com>
2016-11-28 22:11:03 +1000

    Refactored most nested functions to be file scope

--- y_gcc.c
+++ y_gcc.c
@@ -4,57 +4,63 @@
 
 typedef uintmax_t (*func)( uintmax_t ) ; 
 
+                              /* wrapping the function pointer type in a struct 
+                                 is necessary because C does not support a 
+                                 function pointer type that has its own type 
+                                 as an argument */
 typedef struct wrap {
    void (*_)( struct wrap , void (*)( func ) ) ;
 } wrap;
 
 typedef void (*callback)( func ) ;
 
-void show( uintmax_t value ) {
+
+static void y( wrap gen , callback cb ) {
+   gen._(   gen 
+          , ({
+               void _( func f ) {
+                  cb( f ) ;
+               };
+               _;
+            })
+   );
+}
+
+
+static void factorial_improver( wrap partial , callback cb ) {
+   cb( ({ 
+            uintmax_t _( uintmax_t n ) {
+               return n == 0  ? 1 
+                              : ({
+                                    uintmax_t rv = 1;
+                                    partial._(  
+                                          partial 
+                                        , ({
+                                             void _( func f ) {
+                                                rv = n * f( n - 1 );
+                                             };
+                                             _;
+                                          })
+                                    );
+                                    rv;
+                                 }) ;
+            };
+            _;
+   }) );
+}
+
+
+static void show( uintmax_t value ) {
    printf( "%ju" "\n" , value ) ;
 }
 
 
+static void fifty( func factorial ) {
+   show( factorial( 50 ) );
+}
+
+
 int main( int argc , char *argv[] ) {
-
-   void y( wrap gen , callback cb ) {
-         gen._(
-               gen 
-             , ({
-                     void _( func f ) {
-                        cb( f ) ;
-                     };
-                     _;
-               })
-         );
-   };
-
-
-   void factorial_improver( wrap partial , callback cb ) {
-         cb( ({ 
-               uintmax_t _( uintmax_t n ) {
-                  return n == 0  ? 1 
-                                 : ({
-                                       uintmax_t rv = 1;
-                                       partial._(  
-                                             partial 
-                                           , ({
-                                                void _( func f ) {
-                                                   rv = n * f( n - 1 );
-                                                };
-                                                _;
-                                             })
-                                       );
-                                       rv;
-                                    }) ;
-               };
-               _;
-         }) );
-   };
-
-   void fifty( func fx ) {
-      show( fx( 50 ) );
-   };  
 
    y(    (wrap) { ._ = factorial_improver } 
        , fifty
